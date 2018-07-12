13e2ca5207dcb22975bace1c15521695740e0b1b
rf.wangchn <rf.wangchn@gmail.com>
2018-07-12 11:06:00 +0800

    新增三个落地页占位文件;

---
+++ js/single07.js
@@ -0,0 +1,14 @@
+/**
+ * @file name    : single05.js
+ * @created time : 2018-07-12
+ * @author       : 
+ * @email        : 
+ * @github       : 
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    manerr   : 50,
+  newcap : true, nomen   : true, plueplue : true
+  regexp : true, sloppy  : true, vars     : true,
+  white  : true
+*/
